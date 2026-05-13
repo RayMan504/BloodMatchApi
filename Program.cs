c80685c5a9e657df05edd02c8911ba52062a1361
RayMan504 <uraeusmillet@gmail.com>
2026-05-13 16:33:09 -0500

    remove mvc middleware and cshtml view

--- Program.cs
+++ Program.cs
@@ -2,14 +2,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddControllersWithViews();
 builder.Services.AddSingleton<BloodMatchService>();
 
 var app = builder.Build();
 
 app.MapControllers();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.Run();
