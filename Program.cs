1d572f3de558e66f60f64836754780b97293add2
RayMan504 <uraeusmillet@gmail.com>
2026-05-13 11:35:43 -0500

    handle home controller routing

--- Program.cs
+++ Program.cs
@@ -2,9 +2,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddControllersWithViews();
 builder.Services.AddSingleton<BloodMatchService>();
 
 var app = builder.Build();
 
 app.MapControllers();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.Run();
